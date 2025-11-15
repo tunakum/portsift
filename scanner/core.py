fe9a91ea78af7596973a487ce577be6faf25995a
Tunahan Kum <tunalogyy@gmail.com>
2025-11-15 04:17:22 +0300

    add port/ports scanning to core.py

---
+++ scanner/core.py
@@ -0,0 +1,24 @@
+import socket
+
+def scan_port(ip, port):
+    
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        
+        result = sock.connect_ex((ip, port))
+        sock.close()
+        
+        return result == 0
+    
+    except Exception:
+        return False
+
+def scan_ports(ip, ports):
+    
+    open_ports = []
+    
+    for port in ports:
+        if scan_port(ip, port):
+            open_ports.append(port)
+    return open_ports
